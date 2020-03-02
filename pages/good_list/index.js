c1a0b43f27d8254390862f89c58f6a9f99d2f3fd
ohmyray <ohmyray@163.com>
2020-03-02 22:07:01 +0800

    feat: 搜索第一版

--- pages/good_list/index.js
+++ pages/good_list/index.js
@@ -6,7 +6,12 @@ Page({
      * 页面的初始数据
      */
     data: {
-        keyword:''
+        keyword: '',
+        goodsMap: [],
+        goodList: [],
+        pageNum: 1,
+        pageSize: 10,
+        total: 0
     },
 
     /**
@@ -14,22 +19,40 @@ Page({
      */
     onLoad: function (options) {
         // console.log(options); // 拿到页面中的路由传递参数
-        let {keyword} = options
+        let {
+            keyword
+        } = options
         this.setData({
             keyword
         })
         this.findGoodList()
     },
 
-    findGoodList(){
+    findGoodList() {
         request({
-            url: '/goods/qsearch',
+            url: "/goods/search",
             data: {
-                query: this.data.keyword,   // 关键字
-                // pagesize: 10
+                query: this.data.keyword, // 关键字
+                pagenum: this.data.pageNum,
+                pagesize: this.data.pageSize
             }
-        }).then(({data: res})=>{
-            console.log(res);
+        }).then(({
+            data: res
+        }) => {
+            this.setData({
+                total: res.message.total
+            })
+            let goodList = res.message.goods
+                .map(v => {
+                    // 给价格保留两个小数点
+                    v.goods_price = Number(v.goods_price).toFixed(2);
+                    return v
+                })
+            console.log(goodList)
+            this.setData({
+                goodList: goodList,
+                goodsMap: res.message
+            })
         })
     },
 
@@ -72,7 +95,13 @@ Page({
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
-
+        let pageNum = this.data.pageNum;
+        pageNum++
+        this.setData({
+            pageNum
+        })
+        
+        this.findGoodList()
     },
 
     /**
