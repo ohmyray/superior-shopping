bcf47a1f129c207d0b2322e473fd32134adcc45a
ohmyray <ohmyray@163.com>
2020-03-05 13:47:20 +0800

    feat: 完成搜索页面布局，基本效果

--- pages/search/index.js
+++ pages/search/index.js
@@ -5,7 +5,8 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+      isCancel: false,
+      inputValue: ''
     },
 
     /**
@@ -14,53 +15,22 @@ Page({
     onLoad: function (options) {
 
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
+    handleCancel() {
+      this.setData({
+        isCancel: !this.data.isCancel
+      })
     },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
+    handleSearchFocus(e){
+      this.setData({
+        isCancel: false
+      })
     },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    bindKeyInput(e){
+      this.setData({
+        inputValue: e.detail.value
+      })
+      console.log(this.data.inputValue);
+      
     }
+  
 })
