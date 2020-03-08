0ab0d01116f48b3b70f07464c54ef3c5b33e19da
ohmyray <ohmyray@163.com>
2020-03-08 16:16:04 +0800

    feat: 基本数据渲染

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,15 +5,39 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        cartList: [],
+        cartNumber: 0
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        this.getCart(); // 获取购物车数据
     },
+
+    // 获取购物车数据
+    getCart() {
+        let cartList = wx.getStorageSync('cart') || [];
+        let cartNumber = 0;
+        cartList.map(item => {
+            cartNumber += item.number
+            return cartNumber
+        })
+        this.setData({
+            cartList,
+            cartNumber
+        })
+    },
+
+    // 从购物车去对应商品详情
+    handleGoDetail(e){
+        const {id} = e.currentTarget.dataset;
+        wx.navigateTo({
+           url: '/pages/good_detail/index?id=' + id
+        })
+    },
+    
 
     /**
      * 生命周期函数--监听页面初次渲染完成
@@ -26,7 +50,7 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
+        this.getCart(); // 获取购物车数据
     },
 
     /**
